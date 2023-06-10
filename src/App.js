1cd98d159003848958a0d0b0809b5b558e4157ff
Jay King <jayriverking@gmail.com>
2023-06-10 13:03:50 -0400

    add classInfo js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentList from './components/StudentList';
+import ClassInfo from './components/ClassInfo';
+
 
 function App() {
+  const studentList = <StudentList></StudentList>
+  const classInfo = <ClassInfo></ClassInfo>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Attendance: First student list</h1>
+      {studentList}
+      <h2>Class Info!</h2>
+      {classInfo}
+    </main>
   );
 }
 
